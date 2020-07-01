cdc270ea3d7b12928a64ea7d69be5e7d290c33fe
Fabio Fernandes <FFernandes@RosenInspection.net>
2020-07-01 09:55:09 -0300

    updating dataset manually and testing update.py

--- update.py
+++ update.py
@@ -19,20 +19,20 @@ sched = BlockingScheduler()
 def scheduled_job():
     #Reading data section
     ## Reading info from Brasil.IO
-    req = Request('https://brasil.io/dataset/covid19/caso/?format=csv', headers={'User-Agent': 'Mozilla/5.0'})
-    data_covidbr = pd.read_csv(urlopen(req), 
-                      dtype = {'city_ibge_code': 'object'},
-                      parse_dates = ['date'])
+    #req = Request('https://brasil.io/dataset/covid19/caso/?format=csv', headers={'User-Agent': 'Mozilla/5.0'})
+    #data_covidbr = pd.read_csv(urlopen(req), 
+    #                  dtype = {'city_ibge_code': 'object'},
+    #                  parse_dates = ['date'])
 
-    data_covidbr_city = data_covidbr[(data_covidbr['place_type'] == 'city')]
+    #data_covidbr_city = data_covidbr[(data_covidbr['place_type'] == 'city')]
     
     now = datetime.now()
     now_string = now.strftime("%d/%m/%Y %H:%M:%S")
     
        
     # Saving files
-    path_to_save = 'Tables/covid19original.csv'
-    data_covidbr_city.to_csv(path_to_save)
+    #path_to_save = 'Tables/covid19original.csv'
+    #data_covidbr_city.to_csv(path_to_save)
     
     file = open('last_update.txt', 'w')
     file.write(now_string)
