908434f611b6825db2285ba05c0f49b186a34eeb
fabioFernandesBR <fabio.bp.fernandes@gmail.com>
2020-07-01 22:32:15 -0300

    updating the link to dataset compressed with gz

--- update.py
+++ update.py
@@ -15,36 +15,39 @@ from datetime import datetime
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', hours=4)
+@sched.scheduled_job('interval', hours=2)
 def scheduled_job():
+    
+
     #Reading data section
     ## Reading info from Brasil.IO
-    #req = Request('https://brasil.io/dataset/covid19/caso/?format=csv', headers={'User-Agent': 'Mozilla/5.0'})
-    #data_covidbr = pd.read_csv(urlopen(req), 
-    #                  dtype = {'city_ibge_code': 'object'},
-    #                  parse_dates = ['date'])
+    url = 'https://data.brasil.io/dataset/covid19/caso.csv.gz'
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    data_covidbr = pd.read_csv(urlopen(req), 
+                      dtype = {'city_ibge_code': 'object'},
+                      parse_dates = ['date'], 
+                      compression = 'gzip')
 
-    #data_covidbr_city = data_covidbr[(data_covidbr['place_type'] == 'city')]
+    data_covidbr_city = data_covidbr[(data_covidbr['place_type'] == 'city')]
     
     now = datetime.now()
     now_string = now.strftime("%d/%m/%Y %H:%M:%S")
     
        
     # Saving files
-    #path_to_save = 'Tables/covid19original.csv'
-    #data_covidbr_city.to_csv(path_to_save)
+    path_to_save = 'Tables/covid19original.csv'
+    data_covidbr_city.to_csv(path_to_save)
     
-    file = open('last_update.txt', 'w')
-    file.write(now_string)
-    file.close()
+    with open('last_update.txt', 'w') as f:
+        f.write(now_string)
     
+  
     
-    
-sched.start()
+#sched.start()
 
 
-
-
+#Fonte: Secretarias de Saúde das Unidades Federativas, dados tratados por Álvaro Justen e colaboradores/Brasil.IO. 
+        
 
 
 
