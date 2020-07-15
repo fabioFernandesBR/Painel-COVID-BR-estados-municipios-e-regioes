1906949f196e93689313917948829a2fe276b2c4
fabioFernandesBR <fabio.bp.fernandes@gmail.com>
2020-07-15 02:09:01 -0300

    including timeline chart

--- painelcovid19brmunicipios.py
+++ painelcovid19brmunicipios.py
@@ -182,7 +182,7 @@ app.layout = html.Div([
                                        {'label': 'G - Total de casos por 100.000 habitantes', 'value': 'total_casos%'},
                                        {'label': 'H - Total de óbitos por 100.000 habitantes', 'value': 'total_obitos%'},
                                        {'label': 'I - Novos casos por 100.000 habitantes', 'value': 'novos_casos%'},
-                                       {'label': 'K - Novos óbitos por 100.000 habitantes', 'value': 'novos_obitos%'},
+                                       {'label': 'J - Novos óbitos por 100.000 habitantes', 'value': 'novos_obitos%'},
                                        {'label': 'K - Média móvel dos novos casos - 7 dias - por 100.000 habitantes', 'value': 'mm_7dias_novos_casos%'},
                                        {'label': 'L - Média móvel dos novos óbitos - 7 dias - por 100.000 habitantes', 'value': 'mm_7dias_novos_obitos%'}], 
                                value = 'total_casos', 
@@ -191,8 +191,28 @@ app.layout = html.Div([
         
         html.Div(dcc.Graph(id = 'main_map'), #closing dcc.Graph
     #style={'width':1000, 'height':800, 'border':'2px black'}
-                ), ### closing Div-Graph
+                ), ### closing Div-Graph - Map
+                 
+        
+        html.Div(dcc.Graph(id = 'main_lineplot')), #closing dcc.Graph
+    #style={'width':1000, 'height':800, 'border':'2px black'}), ### closing Div-Graph - Line plot
+                 
+        html.Div([html.H6('Legenda:'),
+                  html.Div('A - Total de casos'),
+                  html.Div('B - Total de óbitos'),
+                  html.Div('C - Novos casos'),
+                  html.Div('D - Novos óbitos'),
+                  html.Div('E - Média móvel dos novos casos - 7 dias'),
+                  html.Div('F - Média móvel dos novos óbitos - 7 dias'),
+                  html.Div('G - Total de casos por 100.000 habitantes'),
+                  html.Div('H - Total de óbitos por 100.000 habitantes'),
+                  html.Div('I - Novos casos por 100.000 habitantes'),
+                  html.Div('J - Novos óbitos por 100.000 habitantes'),
+                  html.Div('K - Média móvel dos novos casos - 7 dias - por 100.000 habitantes'),
+                  html.Div('L - Média móvel dos novos óbitos - 7 dias - por 100.000 habitantes')
+                  ]),
     
+        
         html.Div(dash_table.DataTable(id='table', 
                                       export_format = 'csv',
                                       style_cell={'textAlign': 'right'},
@@ -237,31 +257,30 @@ app.layout = html.Div([
                Input('dropdown_geo_level', 'value')])
 def update_figure(info, geo_level):
     ### according to the geo_level, we'll choose the right DataFrame
-    if geo_level == 'states':
-        df = df_estados_hoje
-        locations = locations_states
-        geojson = jdataBra_states
-        featureidkey = 'properties.SIGLA_UF'
-    elif geo_level == 'rgintermed':
+    if geo_level == 'rgintermed':
         df = df_rgint_hoje
         locations = locations_rgintermed
         geojson = jdataBra_rgintermed
         featureidkey='properties.CD_RGINT'
+        text = df['nome_rgint'] + ' / ' + df['sigla'] 
     elif geo_level == 'rgi':
         df = df_rgi_hoje
         locations = locations_rgi
         geojson = jdataBra_rgimed
         featureidkey = featureidkey='properties.CD_RGI'
+        text = df['nome_rgi'] + ' / ' + df['sigla']
     elif geo_level == 'cities':
         df = df_hoje
         locations = locations_mun
         geojson = jdataBra_municipios
         featureidkey = featureidkey='properties.CD_MUN'
+        text = df['nome_mun'] + ' / ' + df['sigla_estado']
     else:  #else select states
         df = df_estados_hoje
         locations = locations_states
         geojson = jdataBra_states
         featureidkey = 'properties.SIGLA_UF'
+        text = df['estado']
         
      
     figure = {'data': [go.Choroplethmapbox(geojson = geojson,
@@ -269,21 +288,20 @@ def update_figure(info, geo_level):
                                            z = df[info],
                                            featureidkey = featureidkey,
                                            colorscale = 'RdPu',
+                                           text = text,
                                            #hovertemplate = '<b>%{customdata[0]}</b><br>z2:%{customdata[1]:.3f} <br>z3: %{customdata[2]:.3f} ',
                                            customdata=df)],
                 'layout': go.Layout(
-                                           #title = 'Mapa de óbitos por estados brasileiro',
+                                           title = 'Mapa da COVID-19 no Brasil',
                                            hovermode = 'closest',
-                                           width = 1000,
-                                           height = 700,
+                                           width = 1500,
+                                           height = 1000,
                                            mapbox={'zoom': 3,
                                                    'style': 'carto-positron',
                                                    'center': {"lat": -15, "lon": -60}})
                                    } #closing figure
                 
-    
-    
-    
+        
     data=df.sort_values(by = info, ascending = False).to_dict('records')
     
     formatlocale_ptBR_int = Format(
@@ -306,7 +324,7 @@ def update_figure(info, geo_level):
             symbol = Symbol.no,
             symbol_prefix = u'R$')
     
-    
+    ### Organizing the format of the table
     if geo_level == 'states':
         columns=[{'id': 'estado', 'name': 'Estado', 'type': 'text'},
                  {'id': 'sigla_estado', 'name': 'Sigla', 'type': 'text'},
@@ -382,13 +400,89 @@ def update_figure(info, geo_level):
                  {'id': 'mm_7dias_novos_obitos%', 'name': 'L', 'type': 'numeric', 'format': formatlocale_ptBR_float},
                  
                  ]
-
-    
-    
+      
                                    
     return figure, data, columns
+
+
+
+
+
+@app.callback(Output('main_lineplot', 'figure'),
+              [Input('dropdown_info', 'value'),
+               Input('dropdown_geo_level', 'value')])
+def update_lineplot(info, geo_level):
+    ### according to the geo_level, we'll choose the right DataFrame
+    if geo_level == 'rgintermed':
+        df = df_rgint_hoje
+        time_df = tdf_rgint
+        place_var = ['cod_rgint', 'nome_rgint', 'sigla']
+    
+    elif geo_level == 'rgi':
+        df = df_rgi_hoje
+        time_df = tdf_rgi
+        place_var = ['cod_rgi', 'nome_rgi', 'sigla']
+    
+    elif geo_level == 'cities':
+        df = df_hoje
+        time_df = tdf
+        place_var = ['codigo_ibge', 'nome_mun', 'sigla_estado']
+    
+    else:  #else select states
+        df = df_estados_hoje
+        time_df = tdf_estados
+        place_var = ['sigla_estado', 'estado']
+        
+     
+    ## creating the line plot
+    top_places = df.sort_values(by = info, ascending = False).head(5)[place_var]
+    
+    if geo_level == 'states':
+        top_places['texto'] = top_places['estado']
+        top_places['local'] = top_places['sigla_estado']
+    else:
+        top_places['texto'] = top_places.iloc[:,1] + ' / ' + top_places.iloc[:,2]
+        top_places['local'] = top_places.iloc[:,0]
     
     
+    data = []
+    for place in top_places.iloc[:,0]:
+        texto = top_places[top_places['local'] == place]['texto'].to_string(index = False)
+        trace = go.Scatter(x = time_df[time_df[place_var[0]] == place]['data'],
+                           y = time_df[time_df[place_var[0]] == place][info],
+                           mode = 'lines+markers',
+                           name = texto)
+        data.append(trace)
+    
+    
+    figure = go.Figure(data=data,
+                       layout=go.Layout(title = 'Curva de evolução da COVID nas localidades selecionadas'))
+
+    figure = figure.update_xaxes(rangeslider_visible=True, 
+                                 range = ['2020-02-25', time_df['data'].max()],
+                                 rangeselector=dict(buttons=list([
+                                         dict(count=7, label="7d", step="day", stepmode="backward"),
+                                         dict(count=14, label="14d", step="day", stepmode="backward"),
+                                         dict(count=1, label="1m", step="month", stepmode="backward"),
+                                         dict(count=6, label="6m", step="month", stepmode="backward"),
+                                         dict(count=1, label="YTD", step="year", stepmode="todate"),
+                                         dict(step="all")])))
+     
+
+
+    figure.update_layout(
+            autosize=False,
+            #width=1500,
+            height=800,
+            margin=dict(
+                l=50,
+                r=50,
+                b=100,
+                t=100,
+                pad=4),
+                    paper_bgcolor="LightSteelBlue")
+                              
+    return figure 
     
     
 if __name__ == '__main__':
